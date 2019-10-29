ea83333a73be0bd8572a434114e2bc35d443ae8e
soejima <soejima@cryptogarage.co.jp>
2019-10-29 18:08:05 +0900

    PR指摘修正

--- include/cfdcore/cfdcore_util.h
+++ include/cfdcore/cfdcore_util.h
@@ -416,7 +416,7 @@ class CFD_CORE_EXPORT RandomNumberUtil {
   static std::vector<uint32_t> GetRandomIndexes(uint32_t length);
   /**
    * ランダムなbool値を生成する.
-   * @param[in/out] random_cache 乱数キャッシュ値
+   * @param[in,out] random_cache 乱数キャッシュ値
    * @return true/false
    */
   static bool GetRandomBool(std::vector<bool> *random_cache);
